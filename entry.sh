f5c011f7b92097ea7bb9b636ec7826c27ab13232
fallenangel3k <fallenangel3k@googlemail.com>
2020-07-10 02:16:06 +0200

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-'''
+"""
 #Update OpenWebRX websocket if we are on port 80 (assuming public https URL)
 if [[ $ORX_WEB_PORT == "80" ]]; then
   echo 'install.sh: Changing ws:// to wss://'
@@ -15,7 +15,7 @@ else
   cd htdocs
   sed -i 's+wss://+ws://+g' openwebrx.js
 fi
-'''
+"""
 
 # run with our custom config file
 cd /usr/src/app/openwebrx
