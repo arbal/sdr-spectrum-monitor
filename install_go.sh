fffd65ea59c7169f1584e696ab137b7eb5fa9187
fallenangel3k <fallenangel3k@googlemail.com>
2020-07-10 05:30:00 +0200

    Update install_go.sh

--- install_go.sh
+++ install_go.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
 wget https://golang.org/dl/go1.14.4.linux-armv6l.tar.gz
-tar -xvf go1.14.4.linux-armv6l.tar.gz
+sudo tar -C /usr/local -xzfgo1.14.4.linux-armv6l.tar.gz
 
 
-sudo chown -R root:root ./go
-sudo mv go /usr/local
-
-export GOPATH=$HOME/go
-export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
 
