a9ac178ce531a4add870daf56eada44d2244d8a3
fallenangel3k <fallenangel3k@googlemail.com>
2020-07-11 11:05:37 +0200

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-#Update OpenWebRX websocket if we are on port 80 (assuming public https URL)
-if [[ $ORX_WEB_PORT == "80" ]]; then
-  cd /usr/src/app/openwebrx
-  sed -i 's+ws://+wss://+g' openwebrx.py
-  cd htdocs
-  sed -i 's+ws://+wss://+g' openwebrx.js
-else
-  cd /usr/src/app/openwebrx
-  sed -i 's+wss://+ws://+g' openwebrx.py
-  cd htdocs
-  sed -i 's+wss://+ws://+g' openwebrx.js
-fi
-
-
-# run with our custom config file
+# run OpenWebRX
 cd /usr/src/app/openwebrx
+git pull
 chmod +x openwebrx.py
-#python openwebrx.py config_alt
 ./openwebrx.py
